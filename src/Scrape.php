d78e3606842ebc2a2fe909212bf98ba315d125ba
Tatenda Munenge <tatemunenge@gmail.com>
2022-07-08 00:44:14 +0200

    implement pagination

--- src/Scrape.php
+++ src/Scrape.php
@@ -11,44 +11,62 @@ class Scrape
     {
         $product = new Product(new DataFormatHelper());
         $document = ScrapeHelper::fetchDocument('https://www.magpiehq.com/developer-challenge/smartphones');
-        $productList = $document->filter('div#products > div > div.product');
+        $paginationlinks = [];
+        /**
+         * Get pagination links
+         */
 
-        foreach ($document->filter('div#products > div > div.product') as $items) 
-        {
-            $node = new Crawler($items); 
-            $product->setTitle( $node->filter( 'span.product-name' )->text( '' )  );
-            $product->setPrice( $node->filter( 'span.product-name' )->text( '' ) );
-            $product->setCapacityMb($node->filter('span.product-capacity')->text('') );
-            $product->setCapacityMb($node->filter('span.product-capacity')->text(''));
-            $product->setImageUrl( $node->filter('img')->attr('src') );
-           
-            $node->filter('div > div.bg-white > div')->each(function (Crawler $child, $index ) use ( &$product ) {
-                //fetch color
-                if( $index == 0)
-                {
-                    $product->setColor($child->filter('div > span')->attr('data-colour'));
-                }
+        foreach ($document->filter('div#pages > div > a') as $items)
+        {   
+            $node = new Crawler($items);
+            $paginationlinks[] = str_replace( '../', 'https://www.magpiehq.com/developer-challenge/', $node->attr('href'));
 
-                if( $index == 1)
-                {  
-                    $product->setPrice($child->text(''));
-                }
-
-                if( $index == 2 )
-                {
-                    $product->setAvailabilityText( $child->text(''));
-                }
-
-                if( $index == 3)
-                {
-                    $product->setShippingText( $child->text(''));
-                }
-            });
-            
-            $this->products[] = $product->getProduct();
         }
 
-        print_r(  $this->products );
+       if( !empty($paginationlinks))
+       {
+            foreach($paginationlinks as $link)
+            {
+                $document = ScrapeHelper::fetchDocument($link);
+
+                foreach ($document->filter('div#products > div > div.product') as $items) 
+                {
+                    echo  "item found" ."\n";
+                    $node = new Crawler($items); 
+                    $product->setTitle( $node->filter( 'span.product-name' )->text( '' )  );
+                    $product->setCapacityMb($node->filter('span.product-capacity')->text('') );
+                    $product->setCapacityMb($node->filter('span.product-capacity')->text(''));
+                    $product->setImageUrl( $node->filter('img')->attr('src') );
+                   
+                    $node->filter('div > div.bg-white > div')->each(function (Crawler $child, $index ) use ( &$product ) {
+                        //fetch color
+                        if( $index == 0)
+                        {
+                            $product->setColor($child->filter('div > span')->attr('data-colour'));
+                        }
+        
+                        if( $index == 1)
+                        {  
+                            $product->setPrice($child->text(''));
+                        }
+        
+                        if( $index == 2 )
+                        {
+                            $product->setAvailabilityText( $child->text(''));
+                        }
+        
+                        if( $index == 3)
+                        {
+                            $product->setShippingText( $child->text(''));
+                        }
+                    });
+                    
+                    $this->products[] = $product->getProduct();
+                }
+            }
+       }
+    
+       print_r(  $this->products );
 
         file_put_contents('output.json', json_encode($this->products));
     }
